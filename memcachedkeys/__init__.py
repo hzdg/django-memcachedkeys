9d520fe0d0611a148b42694d255474f40b280c5c
Matthew Dapena-Tretter <m@tthewwithanm.com>
2013-12-11 10:47:10 -0500

    Add make_keys function

--- memcachedkeys/__init__.py
+++ memcachedkeys/__init__.py
@@ -1,2 +1,25 @@
-# flake8: noqa
 from memcachedkeys.pkgmeta import *
+import re
+from hashlib import md5
+
+
+bad_key_chars = re.compile(ur'[\u0000-\u001f\s]+')
+MAX_LENGTH = 250
+
+
+def make_key(key, key_prefix, version):
+    """
+    Makes a memcached-safe cache key. Use as a KEY_FUNCTION
+
+    """
+    clean_key = bad_key_chars.sub('', key)
+    version_str = str(version)
+    full_key = ':'.join([key_prefix, version_str, clean_key])
+
+    if clean_key != key or len(full_key) > MAX_LENGTH:
+        hashed_key = md5(key).hexdigest()
+        abbrev_keylen = MAX_LENGTH - len(hashed_key) - len('::[]') - len(key_prefix) - len(version_str)
+        new_key = '%s[%s]' % (clean_key[:abbrev_keylen], hashed_key)
+        full_key = ':'.join([key_prefix, version_str, new_key])
+
+    return full_key
